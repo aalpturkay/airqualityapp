bbe88d6d8dcf5f59ded98d43ae42355b215b22b6
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-01 00:53:03 +0300

    Create EnumAqtAirQualityCategoryType.java

---
+++ src/main/java/com/alpturkay/airqualityapp/aqt/enums/EnumAqtAirQualityCategoryType.java
@@ -0,0 +1,11 @@
+package com.alpturkay.airqualityapp.aqt.enums;
+
+public enum EnumAqtAirQualityCategoryType {
+    GOOD,
+    SATISFACTORY,
+    MODERATE,
+    POOR,
+    SEVERE,
+    HAZARDOUS
+    ;
+}
