5c47efa3ddfc66b490fa91c392f454ee3c6a5c84
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-03 01:56:05 +0300

    Update AqtAirQualityDao.java

--- src/main/java/com/alpturkay/airqualityapp/aqt/dao/AqtAirQualityDao.java
+++ src/main/java/com/alpturkay/airqualityapp/aqt/dao/AqtAirQualityDao.java
@@ -1,8 +1,10 @@
 package com.alpturkay.airqualityapp.aqt.dao;
 
 import com.alpturkay.airqualityapp.aqt.entity.AqtAirQuality;
+import com.alpturkay.airqualityapp.cty.entity.CtyCity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AqtAirQualityDao extends JpaRepository<AqtAirQuality, Long> {
-    AqtAirQuality findByCity(String city);
+    AqtAirQuality findByCtyCity(Long city);
+    boolean existsByCtyCity(CtyCity city);
 }
