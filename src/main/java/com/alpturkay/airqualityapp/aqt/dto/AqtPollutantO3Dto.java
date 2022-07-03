546fcd60071cc430adf07eefd050b3d146297b7d
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-04 00:52:31 +0300

    Create AqtPollutantO3Dto.java

---
+++ src/main/java/com/alpturkay/airqualityapp/aqt/dto/AqtPollutantO3Dto.java
@@ -0,0 +1,13 @@
+package com.alpturkay.airqualityapp.aqt.dto;
+
+import com.alpturkay.airqualityapp.aqt.entity.AqtAirQualityCategory;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AqtPollutantO3Dto extends AqtAirQualityCategoryDto {
+    @JsonProperty("O3")
+    private String o3;
+}
