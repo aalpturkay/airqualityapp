703ef8dba29ffd403eddcaca10713a4575875349
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-04 00:52:39 +0300

    Update EnumAqtAirQualityCategoryType.java

--- src/main/java/com/alpturkay/airqualityapp/aqt/enums/EnumAqtAirQualityCategoryType.java
+++ src/main/java/com/alpturkay/airqualityapp/aqt/enums/EnumAqtAirQualityCategoryType.java
@@ -1,11 +1,25 @@
 package com.alpturkay.airqualityapp.aqt.enums;
 
+import lombok.ToString;
+
 public enum EnumAqtAirQualityCategoryType {
-    GOOD,
-    SATISFACTORY,
-    MODERATE,
-    POOR,
-    SEVERE,
-    HAZARDOUS
+    GOOD("Good"),
+    SATISFACTORY("Satisfactory"),
+    MODERATE("Moderate"),
+    POOR("Poor"),
+    SEVERE("Severe"),
+    HAZARDOUS("Hazardous"),
+    NONE("None")
     ;
+
+    private final String readableName;
+
+    EnumAqtAirQualityCategoryType(String readableName) {
+        this.readableName = readableName;
+    }
+
+    public String getReadableName(){
+        return readableName;
+    }
+
 }
