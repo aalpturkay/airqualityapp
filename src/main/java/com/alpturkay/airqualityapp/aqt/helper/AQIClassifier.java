19b49e6122968f9754ae88763641334c39726495
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-05 00:38:08 +0300

    Update AQIClassifier.java

--- src/main/java/com/alpturkay/airqualityapp/aqt/helper/AQIClassifier.java
+++ src/main/java/com/alpturkay/airqualityapp/aqt/helper/AQIClassifier.java
@@ -2,37 +2,30 @@ package com.alpturkay.airqualityapp.aqt.helper;
 
 import com.alpturkay.airqualityapp.aqt.enums.EnumAqtAirQualityCategoryType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@AllArgsConstructor
+
+@Component
 public class AQIClassifier {
 
-    private int goodIndex;
-    private int satisfactoryIndex;
-    private int moderateIndex;
-    private int poorIndex;
-    private int severeIndex;
-    private int hazardousIndex;
-
-    public EnumAqtAirQualityCategoryType classify(final BigDecimal pollutantAverage){
+    public EnumAqtAirQualityCategoryType classify(final AQIValues aqiValues ,final BigDecimal pollutantAverage){
         EnumAqtAirQualityCategoryType category = EnumAqtAirQualityCategoryType.NONE;
-        if (pollutantAverage.compareTo(BigDecimal.valueOf(goodIndex)) < 0){
+        if (pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getGoodIndex())) < 0){
             category = EnumAqtAirQualityCategoryType.GOOD;
-        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(goodIndex+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(satisfactoryIndex)) < 0){
+        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getGoodIndex()+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getSatisfactoryIndex())) < 0){
             category = EnumAqtAirQualityCategoryType.SATISFACTORY;
-        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(satisfactoryIndex+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(moderateIndex)) < 0){
+        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getSatisfactoryIndex()+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getModerateIndex())) < 0){
             category = EnumAqtAirQualityCategoryType.MODERATE;
-        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(moderateIndex+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(poorIndex)) < 0){
+        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getModerateIndex()+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getPoorIndex())) < 0){
             category = EnumAqtAirQualityCategoryType.POOR;
-        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(poorIndex+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(severeIndex)) < 0){
+        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getPoorIndex()+1)) > 0 && pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getSevereIndex())) < 0){
             category = EnumAqtAirQualityCategoryType.SEVERE;
-        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(hazardousIndex)) > 0){
+        } else if (pollutantAverage.compareTo(BigDecimal.valueOf(aqiValues.getHazardousIndex())) > 0){
             category = EnumAqtAirQualityCategoryType.HAZARDOUS;
         }
         return category;
