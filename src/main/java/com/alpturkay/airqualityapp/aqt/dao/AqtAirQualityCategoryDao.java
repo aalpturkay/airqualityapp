d711bc198cb9ec32b2a7c560dac2f3d04b37ca43
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-07 01:43:52 +0300

    Delete AqtAirQualityCategoryDao.java

--- src/main/java/com/alpturkay/airqualityapp/aqt/dao/AqtAirQualityCategoryDao.java
+++
@@ -1,7 +0,0 @@
-package com.alpturkay.airqualityapp.aqt.dao;
-
-import com.alpturkay.airqualityapp.aqt.entity.AqtAirQualityCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AqtAirQualityCategoryDao extends JpaRepository<AqtAirQualityCategory, Long> {
-}
