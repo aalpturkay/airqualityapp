b057fe856e25e42353178840db396154f09f8785
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-06 23:12:45 +0300

    Add get last week method

--- src/main/java/com/alpturkay/airqualityapp/gen/utils/CustomDateUtil.java
+++ src/main/java/com/alpturkay/airqualityapp/gen/utils/CustomDateUtil.java
@@ -1,9 +1,18 @@
 package com.alpturkay.airqualityapp.gen.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class CustomDateUtil {
     public static Long convertDateToUnixTimeStamp(Date date){
         return date.getTime() / 1000;
+    }
+
+
+    public static String getLastWeek(SimpleDateFormat dateFormat){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -7);
+        return dateFormat.format(calendar.getTime());
     }
 }
