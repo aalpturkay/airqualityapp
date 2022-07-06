d32e7618b341b84ad56d7cfb43794aa34039e48b
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-07 01:48:18 +0300

    Add parseStringToDate method for parsing strings to date object

--- src/main/java/com/alpturkay/airqualityapp/gen/utils/CustomDateUtil.java
+++ src/main/java/com/alpturkay/airqualityapp/gen/utils/CustomDateUtil.java
@@ -1,5 +1,6 @@
 package com.alpturkay.airqualityapp.gen.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -9,10 +10,19 @@ public class CustomDateUtil {
         return date.getTime() / 1000;
     }
 
-
     public static String getLastWeek(SimpleDateFormat dateFormat){
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, -7);
         return dateFormat.format(calendar.getTime());
+    }
+
+    public static Date parseStringToDate(String date, SimpleDateFormat dateFormat){
+        Date parsedDate;
+        try {
+            parsedDate = dateFormat.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return parsedDate;
     }
 }
