f17d0a751fbce3e80aab0d1f6c31023bcab9df34
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-07-07 19:35:55 +0300

    Add error messages for date

--- src/main/java/com/alpturkay/airqualityapp/gen/enums/GenErrorMessage.java
+++ src/main/java/com/alpturkay/airqualityapp/gen/enums/GenErrorMessage.java
@@ -4,7 +4,9 @@ package com.alpturkay.airqualityapp.gen.enums;
 import com.alpturkay.airqualityapp.gen.exceptions.BaseErrorMessage;
 
 public enum GenErrorMessage implements BaseErrorMessage {
-    ITEM_ALREADY_EXISTS("This item is already exists"),
+    ITEM_ALREADY_EXISTS("This item is already exists!"),
+    DATE_COULD_NOT_BE_CONVERTED("Date can't be null!"),
+    DATE_COULD_NOT_BE_PARSED("Date could not be parsed!"),
     ;
 
     private String message;
